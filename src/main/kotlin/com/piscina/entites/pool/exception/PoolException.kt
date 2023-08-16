64a6fd09bbdfd703f88f7a43f71be52feac2fd76
layonCarvalho <layon.300@hotmail.com>
2023-08-15 22:17:50 -0300

    abstracting responsibility from the object

--- src/main/kotlin/com/piscina/entites/pool/exception/PoolException.kt
+++ src/main/kotlin/com/piscina/entites/pool/exception/PoolException.kt
@@ -4,3 +4,8 @@ import org.springframework.http.HttpStatus
 
 
 class PoolPostException(val exception: Throwable, val httpStatusCode: HttpStatus, val codeMessage: String, val messageProperty: String) : RuntimeException()
+class PoolDeleteException(val exception: Throwable, val httpStatusCode: HttpStatus, val codeMessage: String, val messageProperty: String) : RuntimeException()
+
+class PoolPutException(val exception: Throwable, val httpStatusCode: HttpStatus, val codeMessage: String, val messageProperty: String) : RuntimeException()
+
+class PoolGetException(val exception: Throwable, val httpStatusCode: HttpStatus, val codeMessage: String, val messageProperty: String) : RuntimeException()
--- src/main/kotlin/com/piscina/service/pool/PoolService.kt
+++ src/main/kotlin/com/piscina/service/pool/PoolService.kt
@@ -1,21 +1,24 @@
 package com.piscina.service.pool
 
 import com.piscina.entites.pool.domain.PoolEntity
+import com.piscina.entites.pool.dtos.request.RequestDeletePoolEntity
 import com.piscina.entites.pool.dtos.request.RequestPostPoolEntity
+import com.piscina.entites.pool.dtos.request.RequestPutPoolEntity
 import java.util.*
 
 interface PoolService {
 
     fun post( body: RequestPostPoolEntity)
 
-    fun getAll(): List <PoolEntity>
+    fun update(body: RequestPutPoolEntity)
 
-    fun getById( id: Long) : Optional<PoolEntity>
-
-    fun update(id: Long, poolEntity: PoolEntity) : Optional<PoolEntity>
-
-    fun delete(id: Long)
+    fun delete(body: RequestDeletePoolEntity)
 
     fun calculteLiters(poolEntity: PoolEntity): Double
+
+    fun getAll(): List <PoolEntity>
+
+    fun getById( id: Long) : PoolEntity
+
 
 }
